2fb54bbd6b140590ca4c9d70a762349daa5b9163
Shourov <shourov.ahmed@student.ap.be>
2021-03-24 00:55:30 +0100

    Workable homescreen + button to Level1

---
+++ Rocket/Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //public GameObject player;
+    public GameObject homeScreenCanvas;
+    public GameObject gameOverCanvas;
+
+    //private Health healthPlayer;
+
+    public enum GameStates { Playing, GameOver}
+    public GameStates gameState = GameStates.Playing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        homeScreenCanvas.SetActive(true);
+        //if (player == null)
+        //{
+        //    player = GameObject.FindGameObjectWithTag("Player");
+        //}
+        //healthPlayer = player.GetComponent<Health>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //state Machine of the game
+
+        //switch (gameState)
+        //{
+        //    case GameStates.Playing:
+        //        if (!healthPlayer.isAlive)
+        //        {
+        //            gameState = GameStates.GameOver;
+        //            mainCanvas.SetActive(false);
+        //            gameOverCanvas.SetActive(true);
+        //        }
+        //        else
+        //            gameOverCanvas.SetActive(false);
+        //        break;
+
+        //    case GameStates.GameOver:
+        //        break;
+        //    default:
+        //        break;
+        //}
+    }
+}
