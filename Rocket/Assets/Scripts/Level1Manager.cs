c0fc8235096665962802f4b76edbb42ddb0b698d
Shourov <shourov.ahmed@student.ap.be>
2021-03-24 02:22:50 +0100

    Added Canvas to continue to Level2
    With debug log and button so that you dont automatically go to the next level

---
+++ Rocket/Assets/Scripts/Level1Manager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1Manager : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject LevelCompleteCanvas;
+
+    private bool Landed;
+    private Rigidbody body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Rocket");
+        }
+        body = player.GetComponent<Rigidbody>();
+        Landed = player.gameObject.GetComponent<Health>().landed;
+        LevelCompleteCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Landed = player.gameObject.GetComponent<Health>().landed;
+        if (Landed)
+        {
+            LevelCompleteCanvas.SetActive(true);
+            body.velocity = Vector3.zero;
+            body.angularVelocity = Vector3.zero;
+        }
+    }
+}
