2892ae604fcd3a6ac45eca57606b294387bc18c1
Shourov <shourov.ahmed@student.ap.be>
2021-03-26 22:21:58 +0100

    Fixed prefabs + fundaments Lvl2
    Different prefabs for level 1 and level 2 rocket + StrafeRocket script for level 2

--- Rocket/Assets/Scripts/StrafeRocket.cs
+++ Rocket/Assets/Scripts/StrafeRocket.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveRocket : MonoBehaviour
+public class StrafeRocket : MonoBehaviour
 {
     private Rigidbody rocket;
     // Start is called before the first frame update  
@@ -10,33 +10,27 @@ public class MoveRocket : MonoBehaviour
     {
         rocket = GetComponent<Rigidbody>();
     }
-    Vector3 Vec;
-    
+
     public int force = 10;
-    public float left = -1f;
-    public float right = 1f;
-    //public int speed = 20;
+    public int speed = 10;
+
     // Update is called once per frame  
     void Update()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            //rocket.AddForce(transform.up * force);
             rocket.AddRelativeForce(transform.up * force);
-        }          
-        // Extra parameter to stop rocket from rotating cuz the freezeRotation kept getting overwritten by this script
-        if (Input.GetKey(KeyCode.LeftArrow) && !rocket.GetComponent<Health>().landed)
-        {
-            rocket.transform.Rotate(left, 0f, 0f);
-        }
-        if (Input.GetKey(KeyCode.RightArrow) && !rocket.GetComponent<Health>().landed)
-        {
-            rocket.transform.Rotate(right,0f,0f);
         }
 
-        
-        
-        //Vec = transform.localPosition;        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rocket.transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rocket.transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+     
         //Vec.x += Input.GetAxis("Horizontal") * Time.deltaTime * speed;
         //Vec.y += Input.GetAxis("Jump") * Time.deltaTime * speed;
         //Vec.z += Input.GetAxis("Vertical") * Time.deltaTime * speed;
