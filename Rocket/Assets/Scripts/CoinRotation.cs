275b22de6730145a71e7122a370faf6e3f6ec7fc
Shourov <shourov.ahmed@student.ap.be>
2021-03-27 01:50:15 +0100

    Added pick up coins

---
+++ Rocket/Assets/Scripts/CoinRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float speed = 180f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * speed * Time.deltaTime);
+    }
+}
--- Rocket/Assets/Scripts/MovePlatforms.cs
+++ Rocket/Assets/Scripts/MovePlatforms.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class MovePlatforms : MonoBehaviour
 {
+    private Vector3 startPositon;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPositon = transform.position;
     }
 
     public float speed = 1.5f;
@@ -16,8 +17,34 @@ public class MovePlatforms : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 platform = this.transform.position;
+        Vector3 platform = transform.position;
         float newX = Mathf.Sin(Time.time * speed) * width;
         this.transform.position = new Vector3(newX, platform.y, platform.z);
     }
+
+    // TODO
+
+    //public float speed = 1.5f;
+    //public float width = 10f;
+    //public float timePeriod = 2;
+    //private float timeSinceStart;
+    //private Vector3 pivot;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    pivot = transform.position;
+    //    width /= 2;
+    //    timeSinceStart = (3 * timePeriod) / 4;
+    //}
+
+
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    Vector3 nextPos = transform.position;
+    //    nextPos.x = pivot.x + width + width * Mathf.Sin(((Mathf.PI * 2) / timePeriod) * timeSinceStart);
+    //    timeSinceStart += Time.deltaTime;
+    //    transform.position = nextPos;
+    //}
 }
